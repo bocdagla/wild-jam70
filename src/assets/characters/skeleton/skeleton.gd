9113a13226293375dfd93976b9b07c9dd326ce10
dferri <dferri@dawe.gg>
2024-06-16 09:59:34 +0200

    Add objects layer and get custom data of target position

--- src/assets/characters/skeleton/skeleton.gd
+++ src/assets/characters/skeleton/skeleton.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-@onready var ray_right = $Raycasts/RayCast2DRight
-@onready var ray_left = $Raycasts/RayCast2Left
-@onready var ray_up = $Raycasts/RayCast2DUp
-@onready var ray_down = $Raycasts/RayCast2DDown
+#@onready var ray_right = $Raycasts/RayCast2DRight
+#@onready var ray_left = $Raycasts/RayCast2Left
+#@onready var ray_up = $Raycasts/RayCast2DUp
+#@onready var ray_down = $Raycasts/RayCast2DDown
 
 @onready var ray = $Raycasts/RayCast2DDown
+
 
 var tile_size = 8
 var inputs = {"right": Vector2.RIGHT,
@@ -23,6 +24,15 @@ func _unhandled_input(event):
 
 func move(dir):
 	ray.target_position = inputs[dir] * tile_size
+	var parent = get_parent() as TileMap
+	var cell_coords = parent.local_to_map(position + inputs[dir] * tile_size)
+	var terrain = parent.get_cell_tile_data(0, cell_coords)
+	if (terrain):
+		print("Terrain: ", terrain.get_custom_data("Data"))
+	var object = parent.get_cell_tile_data(1, cell_coords)
+	if (object):
+		print("Objects: ", object.get_custom_data("Data"))
+	
 	ray.force_raycast_update()
 	if !ray.is_colliding():
 		position += inputs[dir] * tile_size
