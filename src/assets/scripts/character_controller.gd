e26b9a6d4759b9efbd184daf4a6fca66095e873c
Bocdagla <tomeu1m1a1@gmail.com>
2024-06-17 16:36:13 +0200

    Add multichar selector

--- src/assets/scripts/character_controller.gd
+++ src/assets/scripts/character_controller.gd
@@ -11,23 +11,15 @@ const OBJECTS_LAYER = 1
 
 @export var controlling: bool = false
 
+signal activated(is_active: bool);
+
 var tile_size = 8
-var inputs = {"right": Vector2i.RIGHT,
-			"left": Vector2i.LEFT,
-			"up": Vector2i.UP,
-			"down": Vector2i.DOWN}
 
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	animated_sprite.play("idle")
 
-func _unhandled_input(event):
-	if controlling:
-		for dir in inputs.keys():
-			if event.is_action_pressed(dir):
-				move(dir)
-
-func get_tile(coord, layers = [1, 0]):
+func get_tile(coord: Vector2, layers: Array[int] = [1, 0]) -> TileData:
 	var parent = get_parent() as TileMap
 	for layer in layers:
 		var tile = parent.get_cell_tile_data(layer, coord)
@@ -35,20 +27,20 @@ func get_tile(coord, layers = [1, 0]):
 			return tile
 	return null
 
-func get_object(coords):
+func get_object(coords: Vector2) -> TileData:
 	return get_tile(coords, [OBJECTS_LAYER])
 
-func set_object(coords, source_id, atlas_coords):
+func set_object(coords: Vector2, source_id: int, atlas_coords: Vector2):
 	var parent = get_parent() as TileMap
 	parent.set_cell(OBJECTS_LAYER, coords, source_id, atlas_coords)
 
-func can_move(collision_type):
+func can_move(collision_type: int):
 	return can_move_through.has(collision_type)
 
-func can_push(pushable_array):
+func can_push(pushable_array: Array[TileData]):
 	if pushable_array[0].get_custom_data("Pushable") == 0:
 		return false
-	
+
 	var total_weight = 0
 	for e in pushable_array:
 		total_weight += e.get_custom_data("Pushable")
@@ -62,13 +54,13 @@ func can_push(pushable_array):
 		return false
 	return true
 
-func push_objects(from, dir):
+func push_objects(from: Vector2, dir: Vector2):
 	var parent = get_parent() as TileMap
 	var object_source_id = -1
 	var object_atlas_coords = Vector2i.ZERO
 	var i = 0
 	while true:
-		var cell_coords = from + inputs[dir] * i
+		var cell_coords = from + dir * i
 		var tmp_object_source_id = parent.get_cell_source_id(OBJECTS_LAYER, cell_coords)
 		var tmp_object_atlas_coords = parent.get_cell_atlas_coords(OBJECTS_LAYER, cell_coords)
 		var object = get_object(cell_coords)
@@ -77,18 +69,18 @@ func push_objects(from, dir):
 		if !tile || tile_collision != 0:
 			print("Broke box at", cell_coords)
 			break
-		
+
 		set_object(cell_coords, object_source_id, object_atlas_coords)
 		object_source_id = tmp_object_source_id
 		object_atlas_coords = tmp_object_atlas_coords
 		i += 1
-		
+
 		if !object || object.get_custom_data("Pushable") == 0:
 			break
-		
 
-func get_pushable_objects(from, dir, objects = []):
-	var cell_coords = from + inputs[dir]
+
+func get_pushable_objects(from: Vector2, dir: Vector2, objects: Array[TileData] = []) -> Array[TileData]:
+	var cell_coords = from + dir
 	var object = get_tile(cell_coords)
 	if (object):
 		objects.push_back(object)
@@ -97,12 +89,12 @@ func get_pushable_objects(from, dir, objects = []):
 			return get_pushable_objects(cell_coords,dir,objects)
 	return objects
 
-func move(dir):
+func move(dir: Vector2):
 	var parent = get_parent() as TileMap
 	var from_cell_coords = parent.local_to_map(position)
-	var cell_coords = from_cell_coords + inputs[dir]
+	var cell_coords = from_cell_coords  + Vector2i(dir)
 	var will_move = true
-	
+
 	var tile = get_tile(cell_coords)
 	if tile:
 		var collision = tile.get_custom_data("Collision")
@@ -113,6 +105,11 @@ func move(dir):
 			push_objects(cell_coords, dir)
 	else:
 		printerr("Got null tile")
-	
+
 	if will_move:
-		position += Vector2(inputs[dir] * tile_size)
+		position += Vector2(dir * tile_size)
+
+
+func activate(active: bool):
+	controlling = active;
+	activated.emit(active);
---
+++ src/prefabs/player_input/ingame_player_input_handler.gd
@@ -0,0 +1,50 @@
+extends Node
+
+@export var available_chars: Array[Character];
+@export var current_character_index: int = 0;
+@export var currchar_label = Label;
+
+var inputs = {"right": Vector2i.RIGHT,
+			"left": Vector2i.LEFT,
+			"up": Vector2i.UP,
+			"down": Vector2i.DOWN}
+
+func _ready() -> void:
+	call_deferred("activate_char", available_chars[current_character_index]);
+
+func _unhandled_input(event: InputEvent):
+	if event.is_action_pressed("shoulder_r"):
+		next_char();
+	elif event.is_action_pressed("shoulder_l"):
+		prev_char();
+
+	for dir in inputs.keys():
+		if _handle_movement(event, dir):
+			break;
+
+func _handle_movement(event: InputEvent, dir: String) -> bool:
+	if event.is_action_pressed(dir):
+		available_chars[current_character_index].move(inputs[dir]);
+		return true;
+	return false;
+
+func next_char():
+	var previous_character_index = current_character_index;
+	current_character_index += 1;
+	if current_character_index >= available_chars.size():
+		current_character_index = 0;
+	activate_char(available_chars[current_character_index], available_chars[previous_character_index]);
+
+func prev_char():
+	var previous_character_index = current_character_index;
+	current_character_index -= 1;
+	if current_character_index < 0:
+		current_character_index = available_chars.size() - 1;
+	activate_char(available_chars[current_character_index], available_chars[previous_character_index]);
+
+func activate_char(next: Character, prev: Character = null):
+	if prev:
+		prev.activate(false);
+	next.activate(true);
+	if currchar_label:
+		currchar_label.text = next.name;
